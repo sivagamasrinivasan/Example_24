6c25b31eb4a721a366ce65185b03aaf22ad20a10
sivagamasrinivasan <133671469+sivagamasrinivasan@users.noreply.github.com>
2024-09-23 16:46:24 -0400

    Update Student.java
    comment added

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -14,6 +14,7 @@ public class Student // model the student raw data
    private int sid; // principle "encapsulation"
 private int sem;
     private String CourseDetails;
+    // learn fetch+merge
     /**
      * @return the name
      */
