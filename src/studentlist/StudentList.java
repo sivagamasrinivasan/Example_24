fbc8390bd7a2947eaa007e25423ab9cb11e32d6a
sivagamasrinivasan <133671469+sivagamasrinivasan@users.noreply.github.com>
2024-09-23 16:35:18 -0400

    Update StudentList.java
    size modified

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -17,7 +17,7 @@ public class StudentList {
         // TODO code application logic here
         //user interaction
         // define 3 students and print 
-        Student[] list = new Student[3]; // sid, name 
+        Student[] list = new Student[2]; // sid, name 
         //array of object declaration
         //create object
         Student s1 = new Student();
@@ -32,7 +32,7 @@ public class StudentList {
        // s1,s2,s3 - saved the object in array
        list[0] = s1;
        list[1]=s2;
-       list[2]=s3;
+      // list[2]=s3;
        for(int i=0;i<list.length;i++)
        {
           System.out.println(list[i].getName()+ " "+list[i].getSid());
